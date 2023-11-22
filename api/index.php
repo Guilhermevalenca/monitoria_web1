6c57db6427053935baa12280a25ce43410cd1636
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-11-22 19:04:51 -0300

    restarting branch

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'cors.php';
-
-    $array = [
-        'name' => 'Guilherme',
-        'surname' => 'Valença'
-    ];
-
-    echo json_encode($array);
--- api/todo/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../cors.php';
-
-if($_SERVER['REQUEST_METHOD'] === 'GET') {
-
-    require 'getTodos.php';
-
-}
